faf4da8f65d7f5e89e5f3f02c54a21e0a1e926e2
alisitsky <alisitsky@gmail.com>
2023-11-01 19:09:21 +0100

    config updates

--- src/test/java/com/alisitsky/tests/TestBase.java
+++ src/test/java/com/alisitsky/tests/TestBase.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.attachment;
 
 public class TestBase {
@@ -22,11 +21,14 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
